dd6769e2777dd5eb864ff6917d22eee5c23e26ea
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-10-30 12:59:03 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProjectRoute from "./route";
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectRoute />
+    </div>
+  );
+}
+
+export default App;
